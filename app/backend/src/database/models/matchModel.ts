35f7f18b93bf1d18d86e050327d1295cf6c86304
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-03 15:27:40 -0500

    feat: cria matchModel para tabela matches

---
+++ app/backend/src/database/models/matchModel.ts
@@ -0,0 +1,47 @@
+import { Model, DataTypes } from 'sequelize';
+import Team from './teamModel';
+import db from '.';
+
+class Match extends Model {
+  public id!: number;
+  public homeTeam!: number;
+  public homeTeamGoals!: number;
+  public awayTeam!: number;
+  public inProgress!: number;
+  public teamHome: {teamName: string}
+  public teamAway: {teamName: string}
+}
+
+Match.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+  underscored: true,
+});
+export default Match
