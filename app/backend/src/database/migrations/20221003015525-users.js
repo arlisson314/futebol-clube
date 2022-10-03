e1e5a60655cdc60d5ef9eb94cf6498b19766dfc2
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-02 21:06:27 -0500

    feat: cria migration user

---
+++ app/backend/src/database/migrations/20221003015525-users.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+   /**
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {*import('sequelize').Sequelize} Sequelize 
+   */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users',
+    { 
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        // unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+    },
+    {
+      timestamps: false,
+      underscored: true,
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
