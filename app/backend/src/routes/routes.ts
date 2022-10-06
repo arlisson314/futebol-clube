985f257e484b1e99a3f33a97294c8be5c228cfca
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-05 22:47:43 -0500

    feat: cria rota /login

---
+++ app/backend/src/routes/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import LoginController from '../controllers/loginController';
+import TeamController from '../controllers/teamController';
+
+const routes = Router();
+
+const loginController = new LoginController();
+const teamController = new TeamController();
+
+routes.post('/login', loginController.login);
+routes.get('/login/validate', loginController.admin);
+
+routes.get('/teams', teamController.teams);
+
+export default routes;
