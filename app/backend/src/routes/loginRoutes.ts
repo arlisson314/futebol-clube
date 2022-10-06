33e30ce106d5cdc27d724c447e337d77befe6d00
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-05 19:12:20 -0500

    feat: cria rota /login/validate

--- app/backend/src/routes/loginRoutes.ts
+++ app/backend/src/routes/loginRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import LoginController from '../controllers/loginController';
+import LoginValidatController from '../controllers/loginValidateController';
 
 const login = Router();
 
 const loginController = new LoginController();
+const loginValidatController = new LoginValidatController();
+
 login.post('/', loginController.login);
+login.get('/validate', loginValidatController.admin);
 
 export default login;
