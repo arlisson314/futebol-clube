cb700ebc2e393f621085b1e9915c9185f2f37cbf
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-07 16:06:52 -0500

    feat: cria metodo para filtrar rota  /matches  com querystring

--- app/backend/src/controllers/matchController.ts
+++ app/backend/src/controllers/matchController.ts
@@ -4,14 +4,13 @@ import MatchServices from '../services/matchService';
 export default class MatchComtroller {
   private _matchService = new MatchServices();
 
-  public matches: RequestHandler = async (_req, res) => {
+  matches: RequestHandler = async (req, res) => {
+    const { inProgress } = req.query;
+    if (inProgress) {
+      const { code, data } = await this._matchService.matchesByfilter(inProgress as string);
+      return res.status(code).json(data);
+    }
     const { code, data } = await this._matchService.matches();
     return res.status(code).json(data);
   };
-
-  // public matches: RequestHandler = async (req, res) => {
-  //   const { inProgress } = req.query;
-  //   const { code, data } = await this._matchService.matches(inProgress as string);
-  //   return res.status(code).json(data);
-  // };
 }
--- app/backend/src/services/matchService.ts
+++ app/backend/src/services/matchService.ts
@@ -2,6 +2,7 @@ import StatusCodes from 'http-status-codes';
 import IService from '../interfaces/IServiceResponse';
 import Match from '../database/models/matchModel';
 import Team from '../database/models/teamModel';
+import ErrorCustom from '../Middlewares/errorCustum';
 
 export default class MatchServices {
   private _matchModel = Match;
@@ -16,15 +17,20 @@ export default class MatchServices {
     return { code: StatusCodes.OK, data: matches };
   };
 
-  // public matches = async (inProgress: string): Promise<IService> => {
-  //   const query = inProgress ? JSON.parse(inProgress) : '';
-  //   const matches = await this._matchModel.findAll({
-  //     where: { inProgress: query },
-  //     include: [
-  //       { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
-  //       { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
-  //     ],
-  //   });
-  //   return { code: StatusCodes.OK, data: matches };
-  // };
+  public matchesByfilter = async (inProgress: string): Promise<IService> => {
+    const query = (inProgress === 'true' || inProgress === 'false') ? JSON.parse(inProgress) : null;
+
+    if (query === null) {
+      throw new ErrorCustom(StatusCodes.BAD_REQUEST, 'Invalid query');
+    }
+
+    const matches = await this._matchModel.findAll({
+      where: { inProgress: query },
+      include: [
+        { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
+        { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
+      ],
+    });
+    return { code: StatusCodes.OK, data: matches };
+  };
 }
