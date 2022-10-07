fd91df98a0fff57a18aa5ca4a353e5f54aed0d2a
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-06 22:36:34 -0500

    feat: cria camada de controller para rota /matches

--- app/backend/src/controllers/matchController.ts
+++ app/backend/src/controllers/matchController.ts
@@ -8,4 +8,10 @@ export default class MatchComtroller {
     const { code, data } = await this._matchService.matches();
     return res.status(code).json(data);
   };
+
+  // public matches: RequestHandler = async (req, res) => {
+  //   const { inProgress } = req.query;
+  //   const { code, data } = await this._matchService.matches(inProgress as string);
+  //   return res.status(code).json(data);
+  // };
 }
