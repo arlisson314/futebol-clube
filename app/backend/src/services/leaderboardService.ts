205047aa6444d7268041b6e7accdbc39b7dbf858
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-15 10:53:31 -0500

    fix: cria o arquivo leaderBoardService

--- app/backend/src/services/leaderboardService.ts
+++ app/backend/src/services/leaderboardService.ts
@@ -2,7 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 // import ILeadBoard from '../interfaces/IleadBord';
 import IService from '../interfaces/IServiceResponse';
 import Match from '../database/models/matchModel';
-import Team from '../database/models/teamModel';
+// import Team from '../database/models/teamModel';
 
 export default class LeaderBoardService {
   private _matchModel = Match;
@@ -10,23 +10,34 @@ export default class LeaderBoardService {
   public leadBoardHome = async (): Promise<IService> => {
     const matchs = await this._matchModel.findAll({
       where: { inProgress: false },
-      include: [{ model: Team, as: 'teamHome', attributes: { exclude: ['id'] } }],
+      // include: [{ model: Team, as: 'teamHome', attributes: { exclude: ['id'] } }],
     }) as Match[];
 
     const table = matchs.map((key) => ({
       name: key.teamHome.teamName,
-      // totalPoints: LeaderBoardService.sumPoints(matchs),
+      totalPoints: LeaderBoardService.sumPoints(matchs),
     }));
     // return { code: StatusCodes.OK, data: matchs };
     return { code: StatusCodes.OK, data: table };
   };
 
-  // static sumPoints = (matchs: Match[]) => {
-  //   let points = 0;
-  //   matchs.forEach((cur) => {
-  //     if (cur.homeTeamGoals > cur.awayTeamGoals) { points += 3; }
-  //     if (cur.homeTeamGoals === cur.awayTeamGoals) { points += 1; }
-  //   }, 0);
-  //   return points;
-  // };
+  static sumPoints = (matchs: Match[]) => {
+    let points = 0;
+    matchs.forEach((e) => {
+      if (e.homeTeamGoals > e.awayTeamGoals) { points += 3; }
+      if (e.homeTeamGoals === e.awayTeamGoals) { points += 1; }
+    }, 0);
+    return points;
+  };
 }
+
+// {
+//   "id": 1,
+//   "homeTeam": 16,
+//   "homeTeamGoals": 1,
+//   "awayTeam": 8,
+//   "awayTeamGoals": 1,
+//   "inProgress": false,
+//   "teamHome": {
+//     "teamName": "São Paulo"
+//   }
