e169b1ae23937f128140768d31e54e036691def8
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-16 13:31:07 -0500

    feat: cria camada de controller para rota /leaderboard/away

--- app/backend/src/controllers/leadBoardController.ts
+++ app/backend/src/controllers/leadBoardController.ts
@@ -8,4 +8,9 @@ export default class leadBordController {
     const { code, data } = await this._leadBordService.leadBoardHome();
     return res.status(code).json(data);
   };
+
+  public leadBoardAway: RequestHandler = async (_req, res) => {
+    const { code, data } = await this._leadBordService.leadBoardAway();
+    return res.status(code).json(data);
+  };
 }
