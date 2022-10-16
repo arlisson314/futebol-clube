18e91265b5a245a347b5d161e4ba3d55e459ab74
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-16 12:02:15 -0500

    feat: cria metodo para filtrar as classificações dos times da casa

--- app/backend/src/services/leaderboardService.ts
+++ app/backend/src/services/leaderboardService.ts
@@ -1,43 +1,31 @@
 import { StatusCodes } from 'http-status-codes';
-// import ILeadBoard from '../interfaces/IleadBord';
+import ILeadBoard from '../interfaces/IleadBord';
 import IService from '../interfaces/IServiceResponse';
 import Match from '../database/models/matchModel';
-// import Team from '../database/models/teamModel';
+import queryTeamHomes from '../interfaces/query';
 
 export default class LeaderBoardService {
   private _matchModel = Match;
 
   public leadBoardHome = async (): Promise<IService> => {
-    const matchs = await this._matchModel.findAll({
-      where: { inProgress: false },
-      // include: [{ model: Team, as: 'teamHome', attributes: { exclude: ['id'] } }],
-    }) as Match[];
+    const [matchs] = (await this._matchModel
+      .sequelize?.query(queryTeamHomes)) as [ILeadBoard[], unknown];
 
-    const table = matchs.map((key) => ({
-      name: key.teamHome.teamName,
-      totalPoints: LeaderBoardService.sumPoints(matchs),
-    }));
-    // return { code: StatusCodes.OK, data: matchs };
+    const table = LeaderBoardService.table(matchs);
+
     return { code: StatusCodes.OK, data: table };
   };
 
-  static sumPoints = (matchs: Match[]) => {
-    let points = 0;
-    matchs.forEach((e) => {
-      if (e.homeTeamGoals > e.awayTeamGoals) { points += 3; }
-      if (e.homeTeamGoals === e.awayTeamGoals) { points += 1; }
-    }, 0);
-    return points;
-  };
+  static table = (matchs: ILeadBoard[]) => matchs.map((e) => ({
+    name: e.name,
+    totalPoints: Number(e.totalPoints),
+    totalGames: e.totalGames,
+    totalVictories: Number(e.totalVictories),
+    totalDraws: Number(e.totalDraws),
+    totalLosses: Number(e.totalLosses),
+    goalsFavor: Number(e.goalsFavor),
+    goalsOwn: Number(e.goalsOwn),
+    goalsBalance: Number(e.goalsBalance),
+    efficiency: ((Number(e.totalPoints) / (e.totalGames * 3)) * 100).toFixed(2),
+  }));
 }
-
-// {
-//   "id": 1,
-//   "homeTeam": 16,
-//   "homeTeamGoals": 1,
-//   "awayTeam": 8,
-//   "awayTeamGoals": 1,
-//   "inProgress": false,
-//   "teamHome": {
-//     "teamName": "São Paulo"
-//   }
