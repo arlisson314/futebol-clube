2d6e16e4f6ce5003c208a7303f03e6758dd67009
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-16 12:13:17 -0500

    feat: cria interface para tipar retorno de metodos de classificação dos times

--- app/backend/src/interfaces/IleadBord.ts
+++ app/backend/src/interfaces/IleadBord.ts
@@ -1,12 +1,12 @@
 export default interface ILeadBoard {
   name: string,
-  totalPoints: 9,
-  totalGames: 3,
-  totalVictories: 3,
-  totalDraws: 0,
-  totalLosses: 0,
-  goalsFavor: 9,
-  goalsOwn: 3,
-  goalsBalance: 6,
-  efficiency: '100.00'
+  totalPoints: number,
+  totalGames: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+  goalsBalance: number,
+  efficiency: string
 }
