836c0f46d259ff8fcafd6b68639e47736bb58de0
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-17 17:36:01 -0500

    feat: cria metodo para rota /leaderboard  na camada de service

--- app/backend/src/services/leaderboardService.ts
+++ app/backend/src/services/leaderboardService.ts
@@ -8,21 +8,30 @@ export default class LeaderBoardService {
   private _matchModel = Match;
 
   public leadBoardHome = async (): Promise<IService> => {
-    const [matchs] = (await this._matchModel
-      .sequelize?.query(queryTeams.homes)) as [ILeadBoard[], unknown];
+    const [matchsHome] = (await this._matchModel
+      .sequelize?.query(queryTeams.home)) as [ILeadBoard[], unknown];
 
-    const table = LeaderBoardService.table(matchs);
-
+    const table = LeaderBoardService.table(matchsHome);
     return { code: StatusCodes.OK, data: table };
   };
 
   public leadBoardAway = async (): Promise<IService> => {
-    const [matchs] = (await this._matchModel
-      .sequelize?.query(queryTeams.aways)) as [ILeadBoard[], unknown];
+    const [matchsAway] = (await this._matchModel
+      .sequelize?.query(queryTeams.away)) as [ILeadBoard[], unknown];
 
-    const table = LeaderBoardService.table(matchs);
-
+    const table = LeaderBoardService.table(matchsAway);
     return { code: StatusCodes.OK, data: table };
+  };
+
+  public leadBoar = async (): Promise<IService> => {
+    const { data: matchsHome } = await this.leadBoardHome();
+    const { data: matchsAway } = await this.leadBoardAway();
+
+    const tableAll = LeaderBoardService
+      .tableAll(matchsHome as ILeadBoard[], matchsAway as ILeadBoard[]);
+
+    const sortedTableAll = LeaderBoardService.sortedTeams(tableAll);
+    return { code: StatusCodes.OK, data: sortedTableAll };
   };
 
   static table = (matchs: ILeadBoard[]) => matchs.map((e) => ({
@@ -37,4 +46,35 @@ export default class LeaderBoardService {
     goalsBalance: Number(e.goalsBalance),
     efficiency: ((Number(e.totalPoints) / (e.totalGames * 3)) * 100).toFixed(2),
   }));
+
+  static tableAll = (matchsHome: ILeadBoard[], matchsAway: ILeadBoard[]) => matchsHome
+    .map((mH) => {
+      const awayTeamData = matchsAway.find((mA) => (mH.name === mA.name));
+      const generalTable = {
+        name: mH.name,
+        totalPoints: mH.totalPoints + Number(awayTeamData?.totalPoints),
+        totalGames: mH.totalGames + Number(awayTeamData?.totalGames),
+        totalVictories: mH.totalVictories + Number(awayTeamData?.totalVictories),
+        totalDraws: mH.totalDraws + Number(awayTeamData?.totalDraws),
+        totalLosses: mH.totalLosses + Number(awayTeamData?.totalLosses),
+        goalsFavor: mH.goalsFavor + Number(awayTeamData?.goalsFavor),
+        goalsOwn: mH.goalsOwn + Number(awayTeamData?.goalsOwn),
+        goalsBalance: mH.goalsBalance + Number(awayTeamData?.goalsBalance),
+        efficiency: (((mH.totalPoints + Number(awayTeamData?.totalPoints))
+          / ((mH.totalGames + Number(awayTeamData?.totalGames)) * 3)) * 100).toFixed(2).toString(),
+      };
+      return generalTable;
+    });
+
+  static sortedTeams = (tableAll: ILeadBoard[]) => tableAll.sort((a, b) => {
+    if (a.totalPoints > b.totalPoints) return -1;
+    if (a.totalPoints < b.totalPoints) return 1;
+    if (a.goalsBalance > b.goalsBalance) return -1;
+    if (a.goalsBalance < b.goalsBalance) return 1;
+    if (a.goalsFavor > b.goalsFavor) return -1;
+    if (a.goalsFavor < b.goalsFavor) return 1;
+    if (a.goalsOwn > b.goalsOwn) return -1;
+    if (a.goalsOwn < b.goalsOwn) return 1;
+    return 0;
+  });
 }
